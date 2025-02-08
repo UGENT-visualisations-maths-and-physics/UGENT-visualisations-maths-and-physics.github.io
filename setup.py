63b4e674cccd704d1280ce64523451856964ee5b
Simon-Verbruggen <Simon.Verbruggen@UGent.be>
2025-02-08 16:10:39 +0100

    Setup.py file and __init__.py files for module recognition

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+#1. make virtual environment for python in overarching "UGENT-visualisations-maths-and-physics.github.io" directory
+
+#2. execute:
+# pip install setuptools
+
+#allow working with relative paths:
+#3. execute:
+# pip install -e <path> 
+# with <path>, the path to the overarching 'UGENT-visualisations-maths-and-physics.github.io' directory: for example '/Users/simonverbruggen/Desktop/UGENT-visualisations-maths-and-physics.github.io/
+
+#4. pip install other required packages (if necessary)
+#test
+setup(
+    name='math_visualisations',     # package name
+    version='0.1',
+    packages=find_packages(),
+    install_requires = ['numpy'],   # specifies which packages get installed
+    include_package_data=True,
+    python_requires='>=3.6',        # specify Python version requirement
+)
