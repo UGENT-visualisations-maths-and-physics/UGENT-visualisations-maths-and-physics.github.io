3717aa6f8f0aa4eca7715d7d9a8410d90fe978a4
Simon Verbruggen <Verbruggen.simon@gmail.com>
2025-09-02 09:59:37 +0200

    Consistency update: t instead of x as parameter in parametric curves

--- visualisations/Calculus_III/Compiler_script.py
+++ visualisations/Calculus_III/Compiler_script.py
@@ -5,7 +5,7 @@ from visualisations.python_scripts.TexCompiler import tex_compiler
 Analyse_compiler = tex_compiler(folder="Calculus_III")
 Analyse_compiler.create_standalone_tex_files(reset=False)
 Analyse_compiler.create_summary_file(title="Calculus_III", authors="Felix Claeys, Brecht Verbeken, Simon Verbruggen")
-#Analyse_compiler.export_for_syllabus()      # export standalone files for syllabus integration
+Analyse_compiler.export_for_syllabus()      # export standalone files for syllabus integration
 #%%
 "Ensure all changes are committed!"
 Analyse_compiler.push_content_to_gh_pages()
