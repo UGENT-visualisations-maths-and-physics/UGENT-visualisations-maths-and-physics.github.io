2ab2fae29164023c046e9bd76d46391e7f7f9f3a
Simon Verbruggen <Verbruggen.simon@gmail.com>
2025-09-03 12:35:50 +0200

    Calc III: Finalise compiler (and course)

--- visualisations/Calculus_III/Compiler_script.py
+++ visualisations/Calculus_III/Compiler_script.py
@@ -3,9 +3,9 @@ from visualisations.python_scripts.TexCompiler import tex_compiler
 
 # actually call the class now
 Analyse_compiler = tex_compiler(folder="Calculus_III")
-Analyse_compiler.create_standalone_tex_files(reset=False)
+Analyse_compiler.create_standalone_tex_files(reset=True)
 Analyse_compiler.create_summary_file(title="Calculus_III", authors="Felix Claeys, Brecht Verbeken, Simon Verbruggen")
-#Analyse_compiler.export_for_syllabus()      # export standalone files for syllabus integration
+Analyse_compiler.export_for_syllabus()      # export standalone files for syllabus integration
 #%%
 "Ensure all changes are committed!"
 Analyse_compiler.push_content_to_gh_pages()
