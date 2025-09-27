b2bfcf5c4880a6208727a60386d3c0b3b3f117bc
Simon Verbruggen <Verbruggen.simon@gmail.com>
2025-09-27 17:31:36 +0200

    Try caption for mathematical techniques

--- visualisations/Calculus_III/Compiler_script.py
+++ visualisations/Calculus_III/Compiler_script.py
@@ -5,7 +5,7 @@ from visualisations.z_python_scripts.TexCompiler import tex_compiler
 Analyse_compiler = tex_compiler(folder="Calculus_III")
 Analyse_compiler.create_summary_file(title="Calculus_III", authors="Felix Claeys, Brecht Verbeken, Simon Verbruggen", reset=False)
 #%%
-#Analyse_compiler.export_for_syllabus()      # export standalone files for syllabus integration
+Analyse_compiler.export_for_syllabus()      # export standalone files for syllabus integration
 #%%
 "Ensure all changes are committed!"
 # pushing to the website, only available for people with access to push to the website branch (gh-pages)
--- visualisations/Mathematical_Techniques_For_Engineers_Complex_Analysis/Compiler_script.py
+++ visualisations/Mathematical_Techniques_For_Engineers_Complex_Analysis/Compiler_script.py
@@ -3,10 +3,10 @@ from visualisations.z_python_scripts.TexCompiler import tex_compiler
 
 # actually call the class now
 Analyse_compiler = tex_compiler(folder="Mathematical_Techniques_For_Engineers_Complex_Analysis")
-Analyse_compiler.create_summary_file(title="Mathematical_Techniques_For_Engineers_Complex_Analysis", authors="Felix Claeys, Brecht Verbeken, Simon Verbruggen", reset=True)
+Analyse_compiler.create_summary_file(title="Mathematical_Techniques_For_Engineers_Complex_Analysis", authors="Felix Claeys, Brecht Verbeken, Simon Verbruggen", reset=False)
 #%%
 Analyse_compiler.export_for_syllabus()      # export standalone files for syllabus integration
 #%%
 "Ensure all changes are committed!"
+# pushing to the website, only available for people with access to push to the website branch (gh-pages)
 Analyse_compiler.push_content_to_gh_pages()
-# %%
