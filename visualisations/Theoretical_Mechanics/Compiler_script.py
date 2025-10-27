8ff1b6881772502c8803327e6c0454289760090c
Fylaiks <felix.claeys@ugent.be>
2025-10-27 12:25:11 +0100

    changed size to fit article page

---
+++ visualisations/Theoretical_Mechanics/Compiler_script.py
@@ -0,0 +1,16 @@
+#%% Combine all the Tikz files into one tex file and compile
+# add script to path
+import os, sys
+python_scripts_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "z_python_scripts")
+sys.path.append(python_scripts_path)
+from TexCompiler import tex_compiler
+
+# call the class now
+Analyse_compiler = tex_compiler(folder="Theoretical_Mechanics")
+Analyse_compiler.create_summary_file(title="Theoretical_Mechanics", authors="Felix Claeys, Brecht Verbeken, Simon Verbruggen", reset=False)
+#%%
+Analyse_compiler.export_for_syllabus()      # export standalone files for syllabus integration
+#%%
+"Ensure all changes are committed!"
+# pushing to the website, only available for people with access to push to the website branch (gh-pages)
+Analyse_compiler.push_content_to_gh_pages()
